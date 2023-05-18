61bca699a7cdb04f9edb530aaee2a401f27b7295
Artem Ronenko <artemronenko@gmail.com>
2023-05-18 12:30:55 +0600

    changed diagram, limitations added, songs_collection added, songs_albums deleted

--- dop.sql
+++ dop.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS employee(
 	id serial PRIMARY KEY,
 	name varchar(40) NOT NULL,
-	department varchar(40) NOT NULL,
+	department varchar(40) UNIQUE NOT NULL,
 	chief_id integer REFERENCES employee(id)
 )
+
+DROP TABLE employee
